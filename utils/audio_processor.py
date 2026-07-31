fe46ebc4449433a7c46591ba53e370f00db8af7e
Aaditya <aadi006ip@gmail.com>
2026-07-31 23:24:26 +0530

    Update audio_processor.py

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -7,7 +7,8 @@ os.makedirs(DOWNLOAD_DIR,exist_ok = True)
 
 def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
-    ydl_opts = {
+
+ydl_opts = {
         "format": "bestaudio/best",
         "outtmpl": output_path,
         "postprocessors": [
@@ -18,7 +19,15 @@ def download_youtube_audio(url :str) ->str:
             }
         ],
         "quiet": True,
+        "nocheckcertificate": True,
+        "user_agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+        "extractor_args": {
+            "youtube": {
+                "player_client": ["android", "web"]
+            }
+        },
     }
+    
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
